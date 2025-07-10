d016d5944b9f6ae748734c3c537e94516f9c9031
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-10 19:57:12 +0200

    updation

---
+++ password/password.js
@@ -0,0 +1,22 @@
+
+
+    const btn=document.getElementById('btn')
+    btn.addEventListener('click',()=>{
+        const inp=document.getElementById('password')
+         const values=inp.value
+       
+        
+       
+
+        if(/[A-Z]/.test(values) && values.length>8){
+             const hidden = inp.type==='password'
+             inp.type=hidden?'text':'password'
+             btn.textContent=hidden?'Hide':'Show'
+        }
+        else{
+            alert('password must have atleast one captal letter and character is more than 8 character')
+        }
+       
+    })
+
+
